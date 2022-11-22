55ca22e19ade4d46c820268bb896059a9b0df517
andreybyrsa <byrsa2004@bk.ru>
2022-11-22 20:23:50 +0500

    Сделал кнопки в uikit

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -1,7 +1,8 @@
-import { ReactComponent as Moon } from './moon.svg';
-import { ReactComponent as Pill } from './pill.svg';
+import { ReactComponent as Play } from '@Assets/images/icons/play.svg'
+
+import { ReactComponent as Photo } from '@Assets/images/icons/photo.svg'
 
 export default {
-  Moon,
-  Pill,
-};
+  Play,
+  Photo,
+}
--- src/pages/DevPage/DevPage.tsx
+++ src/pages/DevPage/DevPage.tsx
@@ -1,17 +1,27 @@
 import Typography from '@UIkit/Typography'
+import Button from '@UIkit/Button'
 import colors from '@Assets/styles/colors'
+import icons from '@Assets/images/icons'
 
 import './DevPage.scss'
 
 const DevPage = () => {
   return (
-    <Typography
-      className='test'
-      variant='heading-h1'
-      color={colors.blackFont}
-    >
-      DevPage
-    </Typography>
+    <>
+      <Typography
+        className='test'
+        variant='heading-h1'
+        color={colors.blackFont}
+      >
+        DevPage
+      </Typography>
+      <Button type='primary'>Получить код</Button>
+      <Button type='primary' disabled={true}>Получить код</Button>
+      <Button type='secondary'>Получить код</Button>
+      <Button type='primary' icon={icons.Play}>Играть</Button>
+      <Button type='primary' icon={icons.Play} disabled={true}>Играть</Button>
+      <Button type='primary' icon={icons.Photo}>Добавить фото</Button>
+    </>
   )
 }
 
