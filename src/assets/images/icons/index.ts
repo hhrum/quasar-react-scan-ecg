e0c6233f96db0ef6d1208a8d035f6456b921ddbe
8 Hokage <temakravchenko2004@gmail.com>
2022-11-24 01:45:05 +0500

    добавил многа иконок

--- src/assets/images/icons/index.ts
+++ src/assets/images/icons/index.ts
@@ -1,7 +1,21 @@
+import { ReactComponent as Check } from './check.svg'
+import { ReactComponent as HeartActive } from './heart-active.svg'
+import { ReactComponent as HeartDefault } from './heart-default.svg'
+import { ReactComponent as NotificationActive } from './notification-active.svg'
+import { ReactComponent as NotificationDefault } from './notification-default.svg'
 import { ReactComponent as Photo } from './photo.svg'
 import { ReactComponent as Play } from './play.svg'
+import { ReactComponent as Share } from './share.svg'
+import { ReactComponent as Xmark } from './xmark.svg'
 
 export default {
+  Check,
+  HeartActive,
+  HeartDefault,
+  NotificationActive,
+  NotificationDefault,
   Play,
   Photo,
+  Share,
+  Xmark,
 }
